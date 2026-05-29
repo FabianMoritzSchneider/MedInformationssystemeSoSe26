143402b57de838f0cc848c626e17bb0eb609a01d
FabianMoritzSchneider <moritzschneider1999@gmail.com>
2026-05-29 16:47:46 +0200

    added more changes from practice lesson 6

--- src/db.js
+++ src/db.js
@@ -1,17 +1,29 @@
 const mongoose = require('mongoose')
 
 const kittySchema = new mongoose.Schema({
-  name: String
+    name: String,
+    age: Number,
+    isCool: Boolean,
+    favouriteTreats: [], // Altenativ: Array
+    bestToy: {
+        name: String,
+        color: String
+    },
+    toys: [{ 
+        name: String, 
+        color: String 
+    }] 
 });
 
-const Kitten = mongoose.model('Kitten', kittySchema);
+const Kitten = mongoose.model('Kitten', kittySchema)
 
 const main = async () => {
     const db = await mongoose.connect('mongodb://127.0.0.1:27017/test')
     console.log('Connected ...')
 
-    const silence = new Kitten({ name: 'Silence' });
-    console.log(silence.name); // 'Silence'
+    const silence = new Kitten({ name: 'Silence' })
+    const r = await silence.save()
+    console.log(r)
 }
 
 try {
