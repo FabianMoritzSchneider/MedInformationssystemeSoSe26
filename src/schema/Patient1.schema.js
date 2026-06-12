c19ba3b610462c87482842959a95f492a872d5f6
FabianMoritzSchneider <moritzschneider1999@gmail.com>
2026-06-12 17:02:35 +0200

    added function to save patient into mongoDB

--- src/schema/Patient1.schema.js
+++ src/schema/Patient1.schema.js
@@ -1,9 +1,12 @@
 import mongoose from 'mongoose'
+import { nanoid } from 'nanoid'
 
 const Patient1Schema = new mongoose.Schema({
-    patientId: { type: String, required: true, unique: true },
+    patientId: { type: String, required: true, unique: true, index: true, default: nanoid() },
+    timestamp: { type: String, default: Date.now() },
     familyName: { type: String, required: true },
     givenName: [String],
+    gender: { type: String },
     birthDate: { type: String }
 })
 
--- src/service.js
+++ src/service.js
@@ -1,10 +1,24 @@
 import mongoose from 'mongoose'
 
-import Patient1 from './schema/Patient1.schema.js'
+import { nanoid } from 'nanoid'
+
+import Patient1 from './schema/Patient1.schema.js' // faktisch kriegen wir hier eine Klasse aus unserem Schema
 
 const main = async () => {
     const db = await mongoose.connect('mongodb://127.0.0.1:27017/test')
     console.log('Connected...')
+
+    try {
+    const patient = new Patient1({ givenName: ['Tim'] })
+    patient.familyName = 'Kuch'
+    patient.set('gender', 'male')
+    patient.set('familyName', 'Schmidt') // letztes überschreibt immmer
+    // patient.patientId = '124'
+    await patient.save()
+    } catch(e) {
+        console.error('Bad Request')
+        console.error(e)
+    }
 }
 
 try {
